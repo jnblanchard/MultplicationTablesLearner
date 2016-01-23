8f15ab37f1e13b255ce32de5431d78046f57640b
John N Blanchard <jnblanchard@mac.com>
2016-01-23 13:44:47 -0800

    Initial Commit

---
+++ MultiplicationTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiplicationTable
+//
+//  Created by John N Blanchard on 1/23/16.
+//  Copyright © 2016 John N Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
